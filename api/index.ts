80c0fc9fefe9a48675e0cdd5971ec07803d37c75
Odii Daniel <xenithheight@gmail.com>
2025-01-16 01:43:34 +0100

    remove unifiedTopo error mongo

--- api/index.ts
+++ api/index.ts
@@ -12,7 +12,6 @@ app.set('view engine', 'ejs');
 // Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/userDB', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
 });
 
 // Define a User schema and model
